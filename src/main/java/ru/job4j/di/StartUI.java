b96d7ca022c38605c888cc0353990e88a2ca7bb2
Maks Emelianov <mr.maksimemelyanov@ya.ru>
2023-01-10 08:45:11 +0500

    3. @Autowired. Внедрение зависимости [#261276]
    Signed-off-by: Maks Emelianov <mr.maksimemelyanov@ya.ru>

--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,19 +1,26 @@
 package ru.job4j.di;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class StartUI {
 
-    private final Store store;
+    @Autowired
+    private Store store;
 
-    public StartUI(Store store) {
+    /*public StartUI(Store store) {
         this.store = store;
-    }
+    }*/
 
     public void add(String value) {
         store.add(value);
     }
+
+    /*@Autowired
+    public void setStore(Store store) {
+        this.store = store;
+    }*/
 
     public void print() {
         for (String value : store.getAll()) {
